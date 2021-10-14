6ac4d87bae7b9dff41a945f8d8199f707432e6f5
Matthias Willem <willemmatthias@gmail.com>
2021-10-14 17:16:07 +0200

    Api test

--- src/views/AddSensor.vue
+++ src/views/AddSensor.vue
@@ -1,17 +1,27 @@
 <template>
   <v-row>
-    <v-col>
-      <v-text-field label="Name" hide-details="auto" v-model="username"></v-text-field>
-      <v-text-field label="Location" hide-details="auto" v-model="location"></v-text-field>
+    <v-col cols="12" sm="2" md="4">
       <v-btn @click="sendData">
         Send data
-      </v-btn>
-    </v-col>
+      </v-btn></v-col
+    >
+    <v-col cols="12" sm="10" md="8">
+      <v-text-field
+        label="Name"
+        hide-details="auto"
+        v-model="username"
+      ></v-text-field>
+      <v-text-field
+        label="Location"
+        hide-details="auto"
+        v-model="location"
+      ></v-text-field
+    ></v-col>
   </v-row>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
@@ -21,11 +31,20 @@ export default {
     };
   },
   methods: {
-      sendData() {
-          console.log('{"username": "' + this.username + '", "email": "' + this.location + '}')
-          axios.post(`http://localhost:3000/mongo/createdevice`, `{"username": "` + this.username + `", "location": "` + this.location + `}`)
-      }
-  }
+    sendData() {
+      console.log(
+        '{"username": "' + this.username + '", "email": "' + this.location + "}"
+      );
+      axios.post(
+        `http://localhost:3000/mongo/createdevice`,
+        `{"username": "` +
+          this.username +
+          `", "location": "` +
+          this.location +
+          `}`
+      );
+    },
+  },
 };
 </script>
 
