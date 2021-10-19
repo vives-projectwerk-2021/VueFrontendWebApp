c8eaee3115333bd5717cb59f0281b09c0ea59543
Matthias Willem <willemmatthias@gmail.com>
2021-10-19 17:44:10 +0200

    Made addSensor better looking + responsive for mobile

--- src/components/AddSensorPage.vue
+++ src/components/AddSensorPage.vue
@@ -1,44 +1,53 @@
 <template>
-  <v-card color="teal lighten-2">
+  <v-card color="blue lighten-4">
     <v-row>
       <v-col>
         <p class="text-h2 text-center">Add Sensor</p>
-        <p class="text-subtitle-1 text-center">You can add your pulu sensor here!</p>
+        <p class="text-subtitle-1 text-center">
+          You can add your pulu sensor here!
+        </p>
         <v-divider />
       </v-col>
     </v-row>
 
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field
+            label="Device Name"
+            hide-details="auto"
+            v-model="devicename"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <v-text-field
+            label="First Name"
+            hide-details="auto"
+            v-model="firstname"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            label="Last Name"
+            hide-details="auto"
+            v-model="lastname"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+
     <v-row>
-      <v-col>
-        <v-btn @click="sendData" large class="mx-5">
+      <v-col class="text-center">
+        <v-divider />
+        <v-btn @click="sendData" large class="mt-3">
           <v-icon>
             mdi-plus
           </v-icon>
           Add Sensor
-        </v-btn></v-col
-      >
-      <v-col>
-        <v-text-field
-          label="Device Name"
-          hide-details="auto"
-          v-model="devicename"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-text-field
-          label="First Name"
-          hide-details="auto"
-          v-model="firstname"
-        ></v-text-field>
-      </v-col>
-      <v-col>
-        <v-text-field
-          label="Last Name"
-          hide-details="auto"
-          v-model="lastname"
-        ></v-text-field>
+        </v-btn>
       </v-col>
     </v-row>
   </v-card>
