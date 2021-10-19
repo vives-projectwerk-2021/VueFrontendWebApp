86c11e7d39458bc3bc94d77c6564209a9b41111e
Matthias Willem <willemmatthias@gmail.com>
2021-10-19 17:55:51 +0200

    Cleanup scriptcode to component addsensor

--- src/views/AddSensor.vue
+++ src/views/AddSensor.vue
@@ -5,40 +5,12 @@
 </template>
 
 <script>
-import AddSensorPage from '@/components/AddSensorPage'
-import axios from "axios";
+import AddSensorPage from "@/components/AddSensorPage";
 
 export default {
   name: "addSensor",
   components: {
-    AddSensorPage
-  },
-  data() {
-    return {
-      devicename: "",
-      location: "",
-    };
-  },
-  methods: {
-    sendData() {
-      console.log(
-        '{"username": "' +
-          this.devicename +
-          '", "email": "' +
-          this.location +
-          "}"
-      );
-      let json= {name:this.username, location: this.location}
-      axios.post(
-        
-        `${process.env.VUE_APP_BACKEND_BASE_URL}/mongo/createdevice`,
-        json
-        
-      ).then(response=>{console.log(response)})
-      .catch(err=>{console.log(err)})
-    },
+    AddSensorPage,
   },
 };
 </script>
-
-<style></style>
