0b70ae427c0b1d721c81f565be2fd0c24fc73886
Robin Debel <robindebel@hotmail.nl>
2021-10-21 13:06:54 +0200

    add see data button to sensor page

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -3,6 +3,11 @@
       <v-card-title>Sensor 1</v-card-title>
       <v-card-text>Name: Robin</v-card-text>
       <v-card-text>Location: Brugge</v-card-text>
+        <v-card-actions>
+            <v-btn text>
+                See data
+            </v-btn>
+        </v-card-actions>
   </v-card>
 </template>
 
