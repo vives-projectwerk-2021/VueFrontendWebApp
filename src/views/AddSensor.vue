b99478200b4412b9cdb5f7b1c063d598b36aae80
Matthias Willem <willemmatthias@gmail.com>
2021-10-21 20:15:57 +0200

    Trying

--- src/views/AddSensor.vue
+++ src/views/AddSensor.vue
@@ -6,32 +6,41 @@
 
 <script>
 import AddSensorPage from "@/components/AddSensorPage";
+import axios from "axios";
 
 export default {
-  name: "addSensor",
   components: {
     AddSensorPage,
-//   name: 'addSensor',
-//   data() {
-//     return {
-//       username: "",
-//       location: "",
-//     };
-//   },
-//   methods: {
-//     sendData() {
-//       console.log(
-//         '{"username": "' + this.username + '", "location": "' + this.location + "}"
-//       );
-//       let json= {name:this.username, location: this.location}
-//       axios.post(
-        
-//         `${process.env.VUE_APP_BACKEND_BASE_URL}/mongo/createdevice`,
-//         json
-        
-//       ).then(response=>{console.log(response)})
-//       .catch(err=>{console.log(err)})
-//     },
+  },
+  name: "addSensor",
+  data() {
+    return {
+      username: "",
+      location: "",
+    };
+  },
+  methods: {
+    sendData() {
+      console.log(
+        '{"username": "' +
+          this.username +
+          '", "location": "' +
+          this.location +
+          "}"
+      );
+      let json = { name: this.username, location: this.location };
+      axios
+        .post(
+          `${process.env.VUE_APP_BACKEND_BASE_URL}/mongo/createdevice`,
+          json
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
 </script>
