bec8a93435503ae521adf5d6f23f62bbcd2a6527
Matthias Willem <willemmatthias@gmail.com>
2021-10-21 21:58:58 +0200

    Did mad sensorPub look a bit good

--- src/components/SensorPagePub.vue
+++ src/components/SensorPagePub.vue
@@ -1,13 +1,49 @@
 <template>
-  <h1>Testing...</h1>
+  <v-container>
+    <v-card color="red lighten-4" elevation="5">
+      <v-row>
+        <v-col>
+          <p class="text-h2 text-center">{{ devicename }}</p>
+          <v-divider />
+        </v-col>
+      </v-row>
+
+      <v-row class="px-4">
+        <v-col>
+          <v-row>
+            <v-col cols="5">
+              <p class="text-h6 font-weight-medium">First Name:</p>
+            </v-col>
+            <v-col cols="7">
+              <p class="text-h6 font-weight-regular">{{ firstname }}</p>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col>
+          <v-row>
+            <v-col cols="5">
+              <p class="text-h6 font-weight-medium">Last Name:</p>
+            </v-col>
+            <v-col cols="7">
+              <p class="text-h6 font-weight-regular">{{ lastname }}</p>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "SensorPagePub"
-}
+  name: "SensorPagePub",
+  data() {
+    return {
+      devicename: "Serre A",
+      firstname: "Bob",
+      lastname: "Boer",
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
