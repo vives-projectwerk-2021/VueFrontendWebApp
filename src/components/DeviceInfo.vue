98056bd7bad32b8688d53e429480d06fa0a78d28
Robin Debel <robindebel@hotmail.nl>
2021-10-21 13:57:22 +0200

    sensors page now get red from database

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,24 +1,45 @@
 <template>
-  <v-card elevation="4">
-      <v-card-title>Sensor 1</v-card-title>
-      <v-card-text>Name: Robin</v-card-text>
-      <v-card-text>Location: Brugge</v-card-text>
-        <v-card-actions>
-            <v-btn text>
+    <div >
+        <v-card class="my-6" v-for="device in devices" :key="device.id" elevation="5" >
+            <v-card-title>{{ device.name }}</v-card-title>
+            <v-card-text>Location: {{ device.location }}</v-card-text>
+            <v-card-actions>
+                <v-btn text>
                 See data
-            </v-btn>
-        </v-card-actions>
-  </v-card>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script>
+
+import axios from "axios";
+
 export default {
     name: "DeviceInfo",
     data() {
         return {
-
+            devices: null
         }
-    }
-
+    },
+    methods: {
+        getSensors() {
+            var self = this;
+            axios.get(`http://localhost:3000/mongo/alldevices`)
+            .then((response) => {
+                
+                self.devices = response.data;
+            })
+            .catch((error) => {
+                console.log("error a niffauw");
+                console.log(error);
+            })
+        
+        }
+    },
+    created: function(){
+        this.getSensors();
+    },
 }
 </script>
