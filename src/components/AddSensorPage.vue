601802bd9151c1e65bcf23967978fc98e3df5490
Matthias Willem <willemmatthias@gmail.com>
2021-10-22 09:47:35 +0200

    Added Snackbar

--- src/components/AddSensorPage.vue
+++ src/components/AddSensorPage.vue
@@ -1,66 +1,77 @@
 <template>
-  <v-card color="#A9C25D" elevation="5">
-    <v-row>
-      <v-col>
-        <p class="text-h2 text-center">Add Sensor</p>
-        <p class="text-subtitle-1 text-center">
-          You can add your pulu sensor here!
-        </p>
-        <v-divider />
-      </v-col>
-    </v-row>
-
-    <v-container>
+  <v-container>
+    <v-card color="#A9C25D" elevation="5">
       <v-row>
         <v-col>
-          <v-text-field
-            label="Device Name"
-            hide-details="auto"
-            v-model="devicename"
-          ></v-text-field>
+          <p class="text-h2 text-center">Add Sensor</p>
+          <p class="text-subtitle-1 text-center">
+            You can add your pulu sensor here!
+          </p>
+          <v-divider />
         </v-col>
       </v-row>
 
-      <v-row>
-        <v-col>
-          <v-text-field
-            label="Location"
-            hide-details="auto"
-            v-model="location"
-          ></v-text-field>
-        </v-col>
-      </v-row>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="Device Name"
+              hide-details="auto"
+              v-model="devicename"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="Location"
+              hide-details="auto"
+              v-model="location"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="First Name"
+              hide-details="auto"
+              v-model="firstname"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Last Name"
+              hide-details="auto"
+              v-model="lastname"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
 
       <v-row>
-        <v-col>
-          <v-text-field
-            label="First Name"
-            hide-details="auto"
-            v-model="firstname"
-          ></v-text-field>
-        </v-col>
-        <v-col>
-          <v-text-field
-            label="Last Name"
-            hide-details="auto"
-            v-model="lastname"
-          ></v-text-field>
+        <v-col class="text-center">
+          <v-divider />
+          <v-btn @click="sendData" large class="mt-3">
+            <v-icon>
+              mdi-plus
+            </v-icon>
+            Add Sensor
+          </v-btn>
         </v-col>
       </v-row>
-    </v-container>
+    </v-card>
 
-    <v-row>
-      <v-col class="text-center">
-        <v-divider />
-        <v-btn @click="sendData" large class="mt-3">
-          <v-icon>
-            mdi-plus
-          </v-icon>
-          Add Sensor
+    <v-snackbar v-model="snackbar">
+      {{ snackbarText }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="#A9C25D" text v-bind="attrs" @click="snackbar = false">
+          Close
         </v-btn>
-      </v-col>
-    </v-row>
-  </v-card>
+      </template>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
@@ -74,10 +85,14 @@ export default {
       location: "",
       firstname: "",
       lastname: "",
+
+      snackbar: false,
+      snackbarText: `The device: ${this.devicename} has been created!`,
     };
   },
   methods: {
     sendData() {
+      this.snackbar = true;
       // Console log, has to be removed later,from here
       console.log(
         'Device Name: "' +
