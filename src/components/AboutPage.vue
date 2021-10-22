faa349a6f5eb5224038d819b9e2fb10410b6e924
Falk Felix <felix.falk2903@gmail.com>
2021-10-22 11:54:26 +0200

    added hardware and devOps text

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -50,16 +50,24 @@
         </v-col>
       </v-row>
     </v-card> -->
-    <v-card color="white">
+    <v-card color="#F5EEC2">
       <v-container>
         <v-row justify="center">
           <v-col cols="6">
             <div class="text-center">
-              <h1>About the pulu project</h1>
+              <p class="text-h1">About us</p>
               <p>
-                We are a team of 21 students, whose job is to build a
+                We are a team of 21 students, all in our last year, studying ICT at Vives in Bruges.
+                We are going to use our expertise and what we've learned over the years in this all-round group project 
+              </p>
+              <p class="text-h2">About the project</p>
+              <p>
+                Our goal is to build a 
                 functioning moist sensor network with easy implementation to add
                 sensors on the go and by whoever wants or needs it.
+              </p>
+              <p>
+                We got split into 4 groups, being: DevOps,Hardware,Software and Firmware
               </p>
             </div>
           </v-col>
@@ -67,8 +75,11 @@
         <v-row align="center" id="row">
           <v-col>
             <div>
-              <h1 id="journey">The journey to our final product</h1>
-              <p>test</p>
+              <p id="DevOps" class="text-h2">DevOps</p>
+              <p>DevOps is about removing the barriers between traditionally siloed teams, development and operations. 
+                <br>
+                Under a DevOps model, development and operations teams work together across the entire software application life cycle, 
+                from development and test through deployment to operations.</p>
             </div>
           </v-col>
           <v-col>
@@ -76,7 +87,7 @@
               <v-card elevation="2" shaped>
                 <v-parallax
                   id="parallax1"
-                  src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                  src="../assets/DevOps.png"
                   height="350"
                 ></v-parallax>
               </v-card>
@@ -90,7 +101,7 @@
               <v-card elevation="2" shaped>
                 <v-parallax
                   id="parallax2"
-                  src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                  src="../assets/pcb_front_.png"
                   height="350"
                 ></v-parallax>
               </v-card>
@@ -98,19 +109,27 @@
           </v-col>
           <v-col>
             <div>
-              <h1 id="journey2">The journey to our final product</h1>
-              <p>test</p>
+              <p id="hardware" class="text-h2">Hardware</p>
+              <p id ="hardwareText">
+                In team Hardware we focus on the most essential item in this project, the hardware.
+                <br>
+                Our job consists out of designing and assembling the print, that will be used in every complete sensor. We make this from scratch.
+                <br>
+                We have worked with multiple prototypes, so that at the end of the journey we get the best result for this project.
+              </p>
             </div>
           </v-col>
         </v-row>
 
         <v-row align="center" id="row2">
           <v-col>
-            <div>
-              <h1 id="journey">The journey to our final product</h1>
-              <p> We are a team of 21 students, whose job is to build a
+            <div>   
+              <p id="DevOps" class="text-h2">Software</p>
+              <p>
+                We are a team of 21 students, whose job is to build a
                 functioning moist sensor network with easy implementation to add
-                sensors on the go and by whoever wants or needs it.</p>
+                sensors on the go and by whoever wants or needs it.
+              </p>
             </div>
           </v-col>
           <v-col>
@@ -162,10 +181,15 @@ export default {
   margin-left: 20%;
   margin-right: 20%;
 }
-#journey {
+#DevOps {
   margin-top: -10%;
 }
-#journey2 {
-  margin-top: -10%;
+#hardware {
+  margin-top: -5%;
+  margin-left: 3%;
+}
+#hardwareText {
+  margin-left: 3%;
+  margin-right: 41%;
 }
 </style>
