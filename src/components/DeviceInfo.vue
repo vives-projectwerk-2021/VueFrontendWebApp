76bc1971663f052f7f582539110861692f834619
Robin Debel <robindebel@hotmail.nl>
2021-10-22 11:43:19 +0200

    add first name and lastname to sensor card

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -2,6 +2,8 @@
     <div >
         <v-card class="my-6" v-for="device in devices" :key="device.id" elevation="5" >
             <v-card-title>{{ device.name }}</v-card-title>
+            <v-card-text>firstname: {{ device.firstname }}</v-card-text>
+            <v-card-text>lastname: {{ device.lastname }}</v-card-text>
             <v-card-text>Location: {{ device.location }}</v-card-text>
             <v-card-actions>
                 <v-btn text>
