7b876c2271615af51fdf5def02020613c5655385
Thomas Luca <thomasluca008@gmail.com>
2021-11-04 14:30:13 +0100

    improved homepage layout

--- src/components/Sensor.vue
+++ src/components/Sensor.vue
@@ -4,55 +4,34 @@
 
 <template>
   <div>
-    <h2>device: {{liveValues.device_id}}</h2>
-    <vue-ellipse-progress
-      :progress="(100/50)*liveValues.sensors.temperature.air.value"
-      :determinate="false"
-      color="#009E45"
-      empty-color="#324c7e"
-      :size="180"
-      :thickness="5"
-      :empty-thickness="3"
-      lineMode="in 10"
-      :legend-value="liveValues.sensors.temperature.air.value"
-      animation="loop 0 0"
-      fontSize="1.5rem"
-      font-color="#009E45"
-      dash="4"
-      :loading="false"
-    >
-      <span slot="legend-value">°C</span>
-      <p slot="legend-caption">Air temperature</p>
-    </vue-ellipse-progress>
-
-
-    <vue-ellipse-progress
-      class="ml-14"
-      :progress="liveValues.sensors.moisture.level1.value"
-      :determinate="false"
-      color="#009E45"
-      empty-color="#324c7e"
-      :size="180"
-      :thickness="5"
-      :empty-thickness="3"
-      lineMode="in 10"
-      :legend-value="liveValues.sensors.moisture.level1.value"
-      animation="loop 0 0"
-      fontSize="1.5rem"
-      font-color="#009E45"
-      dash="4"
-      :loading="false"
-    >
-      <span slot="legend-value">%</span>
-      <p slot="legend-caption">Moisture level1</p>
-    </vue-ellipse-progress>
+    <h2>device: {{liveValues.device_id}}</h2> 
+      <div class="d-flex flex-wrap">
+        <moisture-card  v-for="(moisture, index) in liveValues.sensors.moisture" :key="moisture.key" 
+          class="ma-1"
+          :percentage="moisture.value"
+          :level="index">
+        </moisture-card>
+        
+        <temperature-card v-for="(temp, index) in liveValues.sensors.temperature" :key="temp.key" 
+          class="ma-1"
+          :temperature="temp.value"
+          :level="index">
+        </temperature-card>
+      </div>
   </div>
 </template>
 
 
 <script>
+import MoistureCard from '@/components/MoistureCard'
+import TemperatureCard from '@/components/TemperatureCard'
+
 export default {
   name: 'Sensor',
   props: ['liveValues'],
+  components: {
+    MoistureCard,
+    TemperatureCard
+  },
 }
 </script>
