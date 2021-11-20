e1f364a29c3b4d1b977558eb0f8dde78893b7d32
Arthur Coupé <arthurcoupe1230@gmail.com>
2021-11-20 17:25:05 +0100

    map frontend

---
+++ src/components/MapPage.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="container">
+    <h1>Map with sensor-devices.</h1>
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+export default {
+  name: "Map",
+  data() {
+    return {
+      center: [51.209348, 3.2246995],
+      data: [],
+      map: null,
+    };
+  },
+  methods: {
+    setupLeafletMap () {
+      this.map = L.map("mapContainer").setView(this.center, 14);
+      L.marker([51.209348, 3.2246995]).addTo(mapLeaflet);
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          accessToken:
+            "pk.eyJ1IjoiYXJ0aHVyMiIsImEiOiJja3c3djNudHMwOGY2Mm5zM2w1d2MzengyIn0.1Ihd1j68_0ipew2AjE85Hw",
+        }
+      ).addTo(this.map);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<style>
+
+#info {
+  width: 18vw;
+  height: 100vh;
+}
+#mapContainer {
+  width: 80vw;
+  height: 100vh;
+}
+</style>
---
+++ src/views/Map.vue
@@ -0,0 +1,20 @@
+<template >
+  <div>
+    <map-page/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+
+import MapPage from '@/components/MapPage'
+
+export default {
+  name: 'Map',
+
+  components: {
+    MapPage
+  },
+}
+</script>
