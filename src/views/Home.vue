929db66339985303576ee4fd3eadacd15aace394
Wout Peirens <wout.peirens@gmail.com>
2021-11-22 14:58:29 +0100

    update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,91 +1,58 @@
 <template>
   <div>
-
-    <div v-if="loadingWS" >
-      <p>Waiting for websocket connection</p>
-      <v-progress-linear
-      indeterminate
-      color="yellow darken-2"
-      ></v-progress-linear>
+    <v-carousel cycle height="300px" hide-delimiter-background  :show-arrows="false" class="rounded-xl">
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+        :src="slide.src"
+      >
+        <v-row class="title fill-height" align="center" justify="center">
+          <div :style="{ background: activeColor }" class="pa-3 text-no-wrap rounded-lg">
+            <h3>Pulu: Smart Self-Sufficient Open Wireless Soil Moisture Sensor</h3>
+          </div>
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+    <br>
+    <div>
+      <v-sheet align="center" border="">
+        <h3 class="mx-16">
+          The Pulu moisture sensor can be used to measure the moisture in the ground. This is very usefull for farmers who want to know the exact value of moisture at different depths. The Pulu sensor is also very user friendly. By scanning a QR code on the sensor you can easily register it on our app. After that you can see the dashboard of your sensor with time lapse of the sensor data.
+        </h3>
+      </v-sheet>
     </div>
-
-    <div v-else>
-      <v-card v-if="!ws || ws.readyState != 1" class="mx-auto text-center pa-6 red accent-1">
-        <h3>No WebSocket Connection</h3>
-        <v-btn @click="retryWsConnection" 
-          elevation="2"
-          large
-          outlined
-          >Retry Connecting
-        </v-btn>
-      </v-card>
-
-      <div v-else-if="hasReceivedValues" :key="sensorsComponentsUpdate">
-        <v-card v-for="device in liveDeviceValues" :key="device.device_id">
-          <Sensor :liveValues="device" class="ma-4" />
-        </v-card>
-      </div>
-
-      <div v-else>
-        <p>Waiting for data...</p>
-        <v-progress-linear
-        indeterminate
-        color="yellow darken-2"
-        ></v-progress-linear>
-      </div>
-
-    </div>
+    <br>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import Sensor from '@/components/Sensor'
-
 export default {
   name: 'Home',
 
-  components: {
-    Sensor
-  },
-
   data() {
     return {
-      loadingWS: true,
+      activeColor: 'white',
+      slides: [
+        {
+          src: require('../img/image_one.jpg')
+        },
+        {
+          src: require('../img/image_two.jpg')
+        },
+        {
+          src: require('../img/image_three.jpg')
+        },
+        {
+          src: require('../img/image_four.jpg')
+        },
+        {
+          src: require('../img/image_five.jpg')
+        }
+      ]
     }
-  },
+  }
 
-  created() {
-    if (this.$store.state.wsReadyState != 1) {
-      setTimeout(() => {
-        this.retryWsConnection()
-        this.loadingWS = false
-      }, 1000)
-    } else {
-      this.loadingWS = false
-    }
-  },
-
-  computed: {
-    liveDeviceValues() {
-      return this.$store.state.liveDeviceValues
-    },
-    sensorsComponentsUpdate() {
-      return this.$store.state.sensorsComponentsUpdate
-    },
-    hasReceivedValues() {
-      return this.$store.state.hasReceivedData
-    },
-    ws() {
-      return this.$store.state.ws
-    },
-  },
-
-  methods: {
-    retryWsConnection() {
-      this.$store.dispatch('tryWsConnection')
-    }
-  },
 }
 </script>
