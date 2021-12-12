aa898b2f2f60069ac6742c2a92fec5320c406dd7
benboydens <r0857165@student.vives.be>
2021-12-12 19:55:32 +0100

    delete sensor button

---
+++ src/components/DeleteSensorButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn @click="deleteSensor" color="error" plain>Delete</v-btn>
+</template>
+
+<script>
+import { Sensors } from "@/api/pulu.js";
+
+export default {
+  name: "DeleteSensorButton",
+  props: {
+    sensorid: String,
+  },
+  methods: {
+    deleteSensor() {
+      // send delete request to backend
+      Sensors.delete_sensor(this.sensorid)
+        .then(() => {
+          this.$router.go(); // refresh page
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
