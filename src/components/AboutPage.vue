72f064ed33e03b5731cb10550067c006d7872359
Falk Felix <felix.falk2903@gmail.com>
2021-12-13 16:38:45 +0100

    changed some small things

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -17,13 +17,13 @@
         <v-col cols="12" md="9">
           
           
-          <p class="text-h5 pb-15">
-            We are a team of 21 students, all in our last year, studying ICT at
+          <p class="text-h5 pb-15 text-justify">
+            We are a team of 21 students, all in our last year, studying ICT- at
             Vives in Bruges. We used our expertise and what we have
             learned over the years in this all-round group project.
           </p>
           <p class="text-h2 text-right pt-15">About the project</p>
-          <v-divider></v-divider>
+          <v-divider class="mb-2"></v-divider>
           <div class="text-center pb-2 pt-5">
             <v-card elevation="2" shaped>
               <v-img
@@ -33,7 +33,7 @@
               <!-- All picture are under the creative common license -->
             </v-card>
           </div>
-          <p class="text-h5 pb-15" >
+          <p class="text-h5 pb-15 text-justify" >
             We build an application, some prototypes and the firmware going with it. So everything that would be needed to scale this project up.
             The idea is that everyone who needs a soil moisture sensor can use our opensource schematics to make their own hardware.
             Then add a device to our user-friendly website, and you should be good to go. Data flows from your device directly to our site where you can see
@@ -46,7 +46,7 @@
 
       <v-row justify="center" class="flex-wrap">
         <v-col cols="12" md="9">
-          <p id="DevOps" class="text-h3 mt-12 mb-0">DevOps</p>
+          <p id="DevOps" class="text-h3 mt-12 mb-0 mb-n4">DevOps</p>
         </v-col>
         <v-col cols="12" md="9">
           <v-divider></v-divider>
@@ -54,7 +54,7 @@
 
         <v-col cols="10" md="5">
           <div>
-            <p class="text-h5">
+            <p class="text-h5 text-justify">
               DevOps is about removing the barriers between traditionally siloed
               teams, development and operations. Under a DevOps model,
               development and operations teams work together across the entire
@@ -80,8 +80,8 @@
       <v-row justify="center">
         <v-col cols="8" md="2">
         </v-col>
-        <v-col cols="10" md="6">
-          <p id="DevOps" class="text-h3 mt-12 mb-0 text-right">Hardware</p>
+        <v-col cols="10" md="7">
+          <p id="DevOps" class="text-h3 mt-12 mb-0 mb-n4 text-right">Hardware</p>
         </v-col>
 
         <v-col cols="12" md="9">
@@ -90,7 +90,7 @@
 
         <v-col order-sm="1" order-md="3" cols="10" md="5">
           <div>
-            <p id="hardwareText" class="text-h5">
+            <p id="hardwareText" class="text-h5 text-justify">
               In team Hardware we focus on the most essential item in this
               project, the hardware. Our job consists out of designing and
               assembling the print, that will be used in every complete sensor.
@@ -115,7 +115,7 @@
 
       <v-row justify="center">
         <v-col cols="12" md="9">
-          <p class="text-h3 mt-12 mb-0">Software</p>
+          <p class="text-h3 mt-12 mb-n4 mb-0">Software</p>
         </v-col>
         <v-col cols="12" md="9">
           <v-divider></v-divider>
@@ -123,7 +123,7 @@
 
         <v-col cols="10" md="5">
           <div>
-            <p class="text-h5">
+            <p class="text-h5 text-justify">
               In team Software we take care of the back- and front-end. The
               backend is formating the data and sending it to the right
               position, which could be a database or the frontend for example.
@@ -149,8 +149,8 @@
       <v-row justify="center" cols="8">
         <v-col cols="8" md="2">
         </v-col>
-        <v-col cols="12" md="6">
-          <p id="DevOps" class="text-h3 mt-12 mb-0 text-right">Firmware</p>
+        <v-col cols="12" md="7">
+          <p id="DevOps" class="text-h3 mt-12 mb-n4 text-right" >Firmware</p>
         </v-col>
         <v-col cols="12" md="9">
           <v-divider></v-divider>
@@ -158,7 +158,7 @@
 
         <v-col order-sm="1" order-md="3" cols="10" md="5">
           <div>
-            <p id="hardwareText" class="text-h5">
+            <p id="hardwareText" class="text-h5 text-justify">
               In team Firmware we make the connection between the hardware and
               backend. For that we make use of the LoRaWAN protocol. The main
               goal of us is to get the data values from the sensors in the field
