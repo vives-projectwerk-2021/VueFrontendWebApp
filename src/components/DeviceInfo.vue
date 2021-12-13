bcbf3421eff0fb7e0ddd07b66036b02d1a1de101
Henry Buysschaert <henry.buysschaert@student.vives.be>
2021-12-13 17:49:28 +0100

    changed deviceinfo layout

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,18 +1,23 @@
 <template>
     <div>
-        <v-card class="mb-6" v-for="device in devicelist" :key="device.id" elevation="5" >
-            <v-card-title>{{ device.devicename }}</v-card-title>
-            <v-card-text>device id: {{ device.deviceid }} </v-card-text>
-            <v-card-text v-if="device.location.place_name">Location: {{ device.location.place_name }}</v-card-text>
-            <v-card-text v-else>Location: [{{ device.location.lat }}, {{ device.location.long }}]</v-card-text>
-            <v-card-actions>
-                <v-btn :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }" text>
-                    See data
-                </v-btn>
-                <v-spacer />
-                <delete-sensor-button :sensorid="device.deviceid"/>
-            </v-card-actions>
-        </v-card>
+        <v-container fluid>
+            <v-row>
+                 <v-col cols="6" v-for="device in devicelist" :key="device.id">
+                        <v-card class="red lighten-5" elevation="5">
+                            <v-card-title class="blue-grey lighten-4"><router-link style="text-decoration:none" color="black" :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }">{{device.devicename}}</router-link></v-card-title>
+                            <v-spacer></v-spacer>
+                            <v-card-text>device id: {{ device.deviceid }} </v-card-text>
+                            <v-card-text v-if="device.location.place_name">Location: {{ device.location.place_name }}</v-card-text>
+                            <v-card-text v-else>Location: [{{ device.location.lat }}, {{ device.location.long }}]</v-card-text>
+
+                            <v-card-actions>
+                            <v-spacer/>
+                            <delete-sensor-button :sensorid="device.deviceid"/>
+                            </v-card-actions>
+                        </v-card>
+                 </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
