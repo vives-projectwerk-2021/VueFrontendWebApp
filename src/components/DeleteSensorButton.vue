8bdfd0ac4248981f546cf6179b1aa9aaf2488c3f
benboydens <r0857165@student.vives.be>
2021-12-13 14:23:41 +0100

    confirmation dialog

--- src/components/DeleteSensorButton.vue
+++ src/components/DeleteSensorButton.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-btn @click="deleteSensor" color="error" plain>Delete</v-btn>
+  <div>
+    <v-btn @click="showDialog" color="error" plain>Delete</v-btn>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title>
+          Message
+        </v-card-title>
+
+        <v-card-text>
+          Are you sure you want to delete this device?
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="deleteSensor">Yes</v-btn>
+          <v-btn color="primary" text @click="dialog = false">No</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -7,10 +28,18 @@ import { Sensors } from "@/api/pulu.js";
 
 export default {
   name: "DeleteSensorButton",
+  data: () => {
+    return {
+      dialog: false,
+    };
+  },
   props: {
     sensorid: String,
   },
   methods: {
+    showDialog() {
+      this.dialog = true;
+    },
     deleteSensor() {
       // send delete request to backend
       Sensors.delete_sensor(this.sensorid)
