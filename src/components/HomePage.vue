c7d08442144cc29f5dcda843b2b86256631be1c2
MatthiasWillem <willemmatthias@gmail.com>
2021-12-14 16:17:36 +0100

    changed color names 2.0

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <br>
+    <br />
 
-    <MapPage/>
+    <MapPage />
 
-    <br>
+    <br />
     <v-divider dark></v-divider>
-    <br>
+    <br />
     <div>
-      <v-sheet align="center" :color="this.$store.state.backgroundColor" dark :style="{ background: activeColor3 }">
+      <v-sheet align="center" :color="this.$store.state.backgroundColor" dark>
         <p class="text-h4">
           Pulu gives an overview of the current groundwater levels in Flanders.
         </p>
@@ -18,145 +18,152 @@
       </v-sheet>
     </div>
 
-    <br>
+    <br />
     <v-divider dark></v-divider>
-    <br>
+    <br />
 
-    <v-carousel cycle height="300px" hide-delimiter-background  :show-arrows="false" class="rounded-xl">
-      <v-carousel-item
-        v-for="(slide, i) in slides"
-        :key="i"
-        :src="slide.src"
-      >
+    <v-carousel
+      cycle
+      height="300px"
+      hide-delimiter-background
+      :show-arrows="false"
+      class="rounded-xl"
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.src">
         <v-row class="title fill-height" align="center" justify="center">
-          <div :style="{ background: activeColor }" class="pa-2 w-50 text-no-wrap rounded-lg">
-            <h5>{{slide.title}}</h5>
+          <div
+            :style="{ background: activeColor }"
+            class="pa-2 w-50 text-no-wrap rounded-lg"
+          >
+            <h5>{{ slide.title }}</h5>
           </div>
         </v-row>
       </v-carousel-item>
     </v-carousel>
-    <br>
+    <br />
     <v-divider></v-divider>
-    <br>
+    <br />
     <v-row class="d-flex justify-space-around pb-5">
       <v-card
-        :color="this.$store.state.accentColorA"
         dark
-        v-for="card in cards" :key="card.id"
+        :color="this.$store.state.accentColorA"
+        v-for="card in cards"
+        :key="card.id"
         width="250px"
         height="200px"
-        :style="{ background: activeColor2 }"
-      >  
-        <h2 class="mt-4 font-weight-regular" style="text-align:center">{{ card.title }}</h2>
-        <p class="mt-5 font-weight-black" :style="{ 'font-size': '50px' }" style="text-align:center" >{{ card.subtitle }}</p>
-      </v-card>  
+      >
+        <h2 class="mt-4 font-weight-regular" style="text-align: center">
+          {{ card.title }}
+        </h2>
+        <p
+          class="mt-5 font-weight-black"
+          :style="{ 'font-size': '50px' }"
+          style="text-align: center"
+        >
+          {{ card.subtitle }}
+        </p>
+      </v-card>
     </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MapPage from "@/components/MapPage.vue"
-import axios from "axios"
+import MapPage from "@/components/MapPage.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
 
   components: {
-    MapPage
+    MapPage,
   },
 
   data() {
     return {
-      activeColor: 'white',
-      activeColor2: '#edf2f7',
-      activeColor3: '#f5f5f5',
+      activeColor: "white",
+      activeColor2: "#edf2f7",
+      activeColor3: "#f5f5f5",
       slides: [
         {
-          src: require('../img/image_one.jpg'),
-          title: "Easy installation"
+          src: require("../img/image_one.jpg"),
+          title: "Easy installation",
         },
         {
-          src: require('../img/image_two.jpg'),
-          title: "Smart Self-Sufficient"
+          src: require("../img/image_two.jpg"),
+          title: "Smart Self-Sufficient",
         },
         {
-          src: require('../img/image_three.jpg'),
-          title: "Open Wireless"
+          src: require("../img/image_three.jpg"),
+          title: "Open Wireless",
         },
         {
-          src: require('../img/image_four.jpg'),
-          title: "Soil Moisture Sensor"
+          src: require("../img/image_four.jpg"),
+          title: "Soil Moisture Sensor",
         },
         {
-          src: require('../img/image_five.jpg'),
-          title: "Created by Vives students"
-        }
+          src: require("../img/image_five.jpg"),
+          title: "Created by Vives students",
+        },
       ],
       cards: [
         {
           id: 0,
           title: "Active sensors",
-          subtitle: "loading"
+          subtitle: "loading",
         },
         {
           id: 1,
           title: "Countries",
-          subtitle: "1"
+          subtitle: "1",
         },
         {
           id: 3,
           title: "Project developers",
-          subtitle: "24"
-        }
-      ]
-      
-    }
+          subtitle: "24",
+        },
+      ],
+    };
   },
   watch: {
     "$store.state.devicelist": {
-      handler: function(nv) {
-        if(nv){
+      handler: function (nv) {
+        if (nv) {
           this.cards[0].subtitle = nv.length;
-          
-          this.getCountries(nv)
+
+          this.getCountries(nv);
         }
-        
       },
-      immediate: true // provides initial (not changed yet) state
-    }
-  },
-  methods:{
-    getMembers(){
-      axios.get("https://api.github.com/orgs/vives-projectwerk-2021/members")
-      .then((members)=>{
-        this.cards[2].subtitle=members.data.length
-      })
-      
+      immediate: true, // provides initial (not changed yet) state
     },
-    getCountries(arr){
+  },
+  methods: {
+    getMembers() {
+      axios
+        .get("https://api.github.com/orgs/vives-projectwerk-2021/members")
+        .then((members) => {
+          this.cards[2].subtitle = members.data.length;
+        });
+    },
+    getCountries(arr) {
+      let countries = [];
 
-      let countries=[];
+      arr.forEach((sensor) => {
+        let country = sensor.location.place_name.split(", ");
+        country = country[country.length - 1];
 
-      arr.forEach(sensor => {
-
-        let country=sensor.location.place_name.split(", ")
-        country=country[country.length-1]
-
-        if(countries.indexOf(country) < 0) {
-            countries.push(country) 
+        if (countries.indexOf(country) < 0) {
+          countries.push(country);
         }
-
       });
 
-      console.log(countries)
+      console.log(countries);
 
-      this.cards[1].subtitle=countries.length
-
-    }
+      this.cards[1].subtitle = countries.length;
+    },
   },
-  created(){
-    this.getMembers()
-  }
-}
+  created() {
+    this.getMembers();
+  },
+};
 </script>
