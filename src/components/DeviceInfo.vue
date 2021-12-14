803e0ae24d662777639f630c6c3d636ad2478c2f
Robin Debel <robindebel@hotmail.nl>
2021-12-14 13:42:50 +0100

    ziet er lelijk uit maar lelijke eendjes zijn ook mooi

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -3,10 +3,10 @@
         <v-container fluid>
             <v-row>
                  <v-col cols="6" v-for="device in devicelist" :key="device.id">
-                        <v-card v-if="device.location.lat && device.location.long" class="red lighten-5" elevation="5">
-                            <v-card-title class="blue-grey lighten-4"><router-link style="text-decoration:none" color="black" :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }">{{device.devicename}}</router-link></v-card-title>
+                        <v-card v-if="device.location.lat && device.location.long" color="#545452" elevation="5">
+                            <v-card-title color="#434342"><router-link style="text-decoration:none" color="black" :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }">{{device.devicename}}</router-link></v-card-title>
                             <v-spacer></v-spacer>
-                            <v-card-text>device id: {{ device.deviceid }} </v-card-text>
+                            <v-card-text >device id: {{ device.deviceid }} </v-card-text>
                             <v-card-text v-if="device.location.place_name">Location: {{ device.location.place_name }}</v-card-text>
                             <v-card-text v-else>Location: [{{ device.location.lat }}, {{ device.location.long }}]</v-card-text>
                             <v-card-text>
