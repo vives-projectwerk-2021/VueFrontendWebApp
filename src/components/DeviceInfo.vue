f8ed09162e8c5f68f10d21f3576229af21cb3a9c
Henry Buysschaert <henry.buysschaert@student.vives.be>
2021-12-19 17:02:36 +0100

    changed deviceinfo layout

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
         <v-container fluid>
+            <v-row class="green darken-3">
+                <v-col>
+                    <p class="text-h2 text-center white--text">Your Devices</p>
+                </v-col>
+            </v-row>
+            <v-row class="text-h4 green darken-3">
+                <v-col>
+                    <p class="text-center white--text">On this page you can consult your devices in more detail</p>
+                </v-col>
+            </v-row>
             <v-row>
-                 <v-col cols="6" v-for="device in devicelist" :key="device.id">
-                        <v-card v-if="device.location.lat && device.location.long" color="#545452" elevation="5">
-                            <v-card-title color="#434342"><router-link style="text-decoration:none" color="black" :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }">{{device.devicename}}</router-link></v-card-title>
+                 <v-col class="green darken-3" cols="6" v-for="device in devicelist" :key="device.id">
+                        <v-card class="rounded-lg" v-if="device.location.lat && device.location.long" color="#545452" elevation="5">
+                            <v-card-title  color="#434342"><router-link  class="white--text" :to="{ name: 'Sensor', params: {deviceId: device.deviceid} }">{{device.devicename}}</router-link></v-card-title>
                             <v-spacer></v-spacer>
-                            <v-card-text >device id: {{ device.deviceid }} </v-card-text>
-                            <v-card-text v-if="device.location.place_name">Location: {{ device.location.place_name }}</v-card-text>
-                            <v-card-text v-else>Location: [{{ device.location.lat }}, {{ device.location.long }}]</v-card-text>
+                            <v-row class="white--text text-h3">
+                                <v-col>
+                                    <v-card-text class="text-h6">Device ID: {{ device.deviceid }} </v-card-text>
+                                    <v-card-text class="text-h6" v-if="device.location.place_name">Location: {{ device.location.place_name }}</v-card-text>
+                                    <v-card-text class="text-h6" v-else>Location: [{{ device.location.lat }}, {{ device.location.long }}]</v-card-text>
+
+                                    <v-card-text class="text-h6">Last Moisture Level: 20% </v-card-text>
+                                    <v-card-text class="text-h6">Last Temperature Value: 18.5°C </v-card-text>
+                                </v-col>
+                            </v-row>
                             <v-card-text>
+                            
                                 <map-lat-long v-if="device.location.lat && device.location.long" 
                                 v-bind:lat="device.location.lat" 
                                 v-bind:long="device.location.long" 
@@ -20,7 +38,7 @@
                                 
                             <v-card-actions>
                             <v-spacer/>
-                            <delete-sensor-button :sensorid="device.deviceid"/>
+                            <delete-sensor-button class="grey darken-4 rounded-xl" :sensorid="device.deviceid"/>
                             </v-card-actions>
                         </v-card>
                  </v-col>
